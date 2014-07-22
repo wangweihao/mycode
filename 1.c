732825aab1e86375d69f9a9a7d310cec5cba614b
wangweihao <578867817@qq.com>
2014-07-22 23:27:23 +0800

    test

---
+++ 1.c
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  1.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年07月22日 23时26分58秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  wangweihao, 578867817@qq.com
+ *        Company:  Class 1304 of Communication
+ *
+ * =====================================================================================
+ */
+
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+
+	test
+	return EXIT_SUCCESS;
+}
+
