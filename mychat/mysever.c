2facc9a1a5ded7c5912818ceee732e55d25ca5c6
wangweihao <578867817@qq.com>
2014-08-08 20:54:03 +0800

    c/s chat updata

---
+++ mychat/mysever.c
@@ -0,0 +1,246 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  serve.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年08月04日 09时12分15秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  wangweihao, 578867817@qq.com
+ *        Company:  Class 1304 of Communication
+ *
+ * =====================================================================================
+ */
+
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <errno.h>
+
+#include "severhead.h"
+#define SERVPORT 4057
+#define BACKLOG 10
+
+
+
+char name[256];
+static int name_i = 0;
+static int ppi = 0;
+pthread_mutex_t number_mutex;
+pthread_mutex_t number2_mutex;
+
+void recv_user_info(int con_fd, char *info)
+{
+	if(recv(con_fd, info, sizeof(info), 0) < 0)
+	{ 
+		perror("recv");
+		exit(1);
+	}
+}
+
+
+
+void exe_in(int *con_fd)
+{
+	int i,j;
+	int flag = 1;
+	char ret[10][256];
+	int s = -1;
+	int newfd[10];
+	int k = -1;
+	char quit_name[256];
+
+	s++;
+	k++;
+	printf("you are the %d people, con_fd:%d, name:%s\n",ppi,*con_fd,name);
+	strncpy(quit_name, name, 40);
+	newfd[k] = *con_fd;
+
+	fflush(stdout);
+
+	memset(tmpname[name_i], 0, 40);
+	recv(newfd[k], tmpname[name_i], 40, 0);
+
+	while(flag)
+	{
+	//	pthread_mutex_lock(&number2_mutex);
+		recv(newfd[k], ret[s], 256, 0);
+		printf("s = %d,*con_fd = %d\n",s,newfd[k]);
+		printf("服务器收到\n");
+		for(i = 0; i < 10; i++)
+		{
+			send(people[i].confd, ret[s], strlen(ret[s]), 0);
+		}
+		printf("s = %d.people[i].confd = %d\n",s,people[i].confd);
+		printf("服务器已发送,%s\n",people[i].name);
+		//pthread_mutex_unlock(&number2_mutex);
+		if(strcmp(ret[s], "bye") == 0)
+		{
+			for(j = 0; j < 10; j++)
+			{
+				if(strcmp(tmpname[j], quit_name) == 0)
+				{
+					memset(tmpname, 0, 40);
+				}
+			}
+			pthread_exit(0);
+		}
+		memset(ret[s], 0, 256);
+	}
+//	name_i++;
+}
+
+
+
+void exe_info(int *con_fd)
+{
+	int i,j;
+	int flag = 1;
+	char ret[10][256];
+	int s = -1;
+	int newfd[10];
+	int k = -1;
+	char quit_name[256];
+
+	s++;
+	k++;
+	printf("you are the %d people, con_fd:%d, name:%s\n",ppi,*con_fd,name);
+	strncpy(quit_name, name, 40);
+	newfd[k] = *con_fd;
+
+	pthread_mutex_lock (&number_mutex);
+	fflush(stdout);
+	memset(tmpname[name_i], 0, 40);
+	recv(newfd[k], tmpname[name_i], 40, 0);
+	printf("接收聊天对象->tmp_name:%s\n",tmpname[name_i]);
+	
+	printf("您要和%s聊天,con_fd = %d\n",tmpname[name_i],newfd[k]);
+	for(i = 0; i < 10; i++)
+	{
+		if(strcmp(tmpname[name_i], people[i].name) == 0)
+		      break;
+	}
+	if(i == 10)
+	{
+		printf("\n抱歉！您查找的人不存在！\n您不能和不存在的人聊天\n请重新输入\n");
+		return;
+	}
+	sleep(2);
+	name_i++;
+	pthread_mutex_unlock (&number_mutex);
+	while(flag)
+	{
+		recv(newfd[k], ret[s], 256, 0);
+		printf("s = %d,*con_fd = %d\n",s,newfd[k]);
+		printf("服务器收到\n");
+		send(people[i].confd, ret[s], strlen(ret[s]), 0);
+		printf("s = %d.people[i].confd = %d\n",s,people[i].confd);
+		printf("服务器已发送,%s\n",people[i].name);
+		if(strcmp(ret[s], "bye") == 0)
+		{
+			for(j = 0; j < 10; j++)
+			{
+				if(strcmp(tmpname[j], quit_name) == 0)
+				{
+					memset(tmpname, 0, 40);
+				}
+			}
+			pthread_exit(0);
+		}
+		memset(ret[s], 0, 256);
+	}
+//	name_i++;
+}
+
+void input(char *buf)
+{
+	int i= 0;
+	char ch;
+	while((ch = getchar()) != '\n')
+	{
+		buf[i] = ch;
+		i++;
+	}
+	buf[i] = '\0';
+}
+
+int main(int argc, char *argv[])
+{
+	int sock_fd, con_fd, client_len;
+	struct sockaddr_in my_addr,client_addr;
+	int i = 0,j;
+	int tmp_confd;
+//	char name[256];
+
+	pthread_t thid;
+
+
+	//初始化结构体套接字
+	for(j = 0; j < 10; j++)
+	{
+		people[j].confd = -1;
+		j++;
+	}
+
+	//创建套接字
+	sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	//初始化
+	memset(&my_addr, 0, sizeof(my_addr));
+	my_addr.sin_family = AF_INET;
+	my_addr.sin_port = htons(SERVPORT);
+	my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	//inet_aton("222.24.51.52", &my_addr.sin_addr);
+	//错误：inet_aton(p,&my_addr.sin_addr);
+
+	//绑定到本客户端
+	bind(sock_fd, (struct sockaddr*)&my_addr, sizeof(struct sockaddr_in));
+
+	//转化为监听套接字
+	if(listen(sock_fd, BACKLOG) < 0)
+	{
+		perror("listen");
+		exit(1);
+	}
+	client_len = sizeof(struct sockaddr_in);
+
+	while(1)
+	{
+		con_fd = accept(sock_fd, (struct sockaddr*)&client_addr, &client_len);
+		memset(name, 0, sizeof(name));
+	
+		
+		//接收连入的用户信息
+		
+		recv_user_info(con_fd, name);
+		if(strcmp(name, "group") == 0)
+		{
+			printf("要开始群聊啦！！\n");
+			pthread_create(&thid, NULL, (void *)exe_in, &con_fd);
+		}
+		else
+		{
+			strcpy(people[ppi].name, name);
+			people[ppi].confd = con_fd;
+			ppi++;
+			printf("%s登陆啦！！\n",name);
+			pthread_create(&thid, NULL, (void *)exe_info, &con_fd);
+		}
+
+	}
+
+	close(sock_fd);
+
+	return EXIT_SUCCESS;
+}
+
---
+++ mychat/register.c
@@ -0,0 +1,411 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  register.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年08月05日 00时47分07秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  wangweihao, 578867817@qq.com
+ *        Company:  Class 1304 of Communication
+ *
+ * =====================================================================================
+ */
+
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <curses.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "severhead.h"
+
+
+void face(void);
+void enter_user(void);
+void apply_user(void);
+void select_content(void);
+void see_friend();
+void see_live_friend();
+void see_group();
+void see_live_group();
+void create_client();
+void enter_group();
+void mod_identify();
+void read_data();
+void init_info(struct Info *user);
+
+
+
+int main(int argc, char *argv[])
+{
+	init_info(user);
+	face();
+	read_data();
+
+	return EXIT_SUCCESS;
+}
+
+void init_info(struct Info *user)
+{
+	int i = 0;
+	int j = 0;
+
+	if(tmp == 0)
+	{
+		printf("初始化\n");
+	}
+	else
+	      return;
+
+	for(i = 0; i < MAX_USER; i++)
+	{
+		user[i].con_fd = -1;
+		user[i].exist = -1;
+		user[i].flag = -1;
+	}
+
+	for(j = 0; j < 5; j++)
+	{
+		group[i].flag = 0;
+		for(i = 0; i < 10; i++)
+		{
+			group[j].member[i].con_fd = -1;
+		}
+	}
+	tmp = -1;
+}
+
+
+
+void read_data()
+{
+	int i,user_num;
+	FILE *fp;
+
+	fp = fopen("user_info.txt", "r");
+
+	//读取文件中的数据，用户信息
+	for(i = 0; i < MAX_USER; i++)
+	{
+		//fread(&user[i], sizeof (struct Info), 1, fp);	
+		//错误，应该用fscanf读取，读写文件必须用一致的格式，或者是文本，或者是二进制。
+	
+		fscanf(fp,"%s %s %s %d %s %s %d %d %d %d",user[i].number, user[i].secret,user[i].name, &user[i].age, user[i].sex, user[i].aut, &user[i].attr, &user[i].exist, &user[i].con_fd, &user[i].flag);
+	}
+
+	//测试代码
+	/*
+	for(user_num = 0; user_num < MAX_USER; ++user_num)
+	{
+		if(user[user_num].exist == -1)
+		      break;
+		printf("激活一位成员\n");
+		sleep(2);
+		fflush (stdout);
+	}
+	printf("user_num = %d\n",user_num);
+	sleep(3);
+	fflush(stdout);
+	for(i = 0; i < user_num; i++)
+	{
+		printf("%s %s %s %s %d %s %d %d %d %d\n",user[i].number, user[i].secret, user[i].name, user[i].aut, user[i].age, user[i].sex, user[i].attr, user[i].exist,  user[i].con_fd, user[i].flag);
+		fflush(stdout);
+	}
+	*/
+
+	fclose(fp);
+}
+
+void face(void)
+{
+	int a;
+
+
+	system("clear");
+	printf("\n");
+	printf("\t\t                      ▁▂▃▄▅▆▇█▇▆▅▄▃▂▁                 \n\n");
+	printf("\t\t                        easy  聊天室                   \n");
+	printf("\t\t                                                       \n");
+	printf("\t\t                                                       \n");
+	printf("\t\t                        1 登   陆                      \n\n");
+	printf("\t\t                                                       \n");
+	printf("\t\t                        2 申请账户                     \n\n");
+	printf("\t\t                                                       \n");
+	printf("\t\t                        0 退   出                      \n\n");
+	printf("\t\t                                                       \n");
+	printf("\t\t                                                       \n");
+
+	printf("请选择:");
+	scanf("%d",&a);
+	switch(a)
+	{
+		case 1:
+			system("clear");
+			enter_user();
+			break;
+		case 2:
+			system("clear");
+			apply_user();
+			break;
+		case 0:
+			system("clear");
+			printf("\n\n\n\n\t\t\t\t谢谢使用,再见！！\n\n\n\n\n");
+			break;
+		default:
+			printf("输入错误！请从新输入..\n");
+	}
+
+}
+
+void enter_user()
+{
+	char secret[20];
+	char user[20];
+	int i = 0, j = 0;
+	char ch;
+	int oldfd, save_fd;
+
+	
+
+
+	printf("\n\n\n\n\t\t\t  帐号:");
+	scanf("%s",user);
+	getchar();
+	printf("\n\n\t\t\t  密码:");
+	scanf("%s",secret);
+	getchar();
+	//if(strcmp() == 0)
+	printf("\n\n\n\n\t\t\t密码正确，欢迎进入...\n");
+	sleep(2);
+	select_content();
+	
+}
+
+//申请帐号，ui是第i个user
+void apply_user(void)
+{
+	int fd;
+	FILE *fp;
+ 	char super[20] = "wangweihao";
+	char super2[20];
+
+	fp = fopen("user_info.txt","at+");
+	if(fp == NULL)
+	{
+		printf("error\n");
+		exit(1);
+	}
+
+	printf("帐  号：");
+	getchar();
+	scanf("%s",user[ui].number);
+	getchar();
+	printf("\n密  码：");
+	scanf("%s",user[ui].secret);
+	getchar();
+	printf("\n姓  名：");
+	scanf("%s",user[ui].name);
+	getchar();
+	printf("\n年  龄：");
+	scanf("%d",&user[ui].age);
+	getchar();
+	printf("\n性  别：");
+	scanf("%s",user[ui].sex);
+	getchar();
+	printf("\n个 性 签 名：");
+	scanf("%s",user[ui].aut);
+	getchar();
+	printf("\n0 普通用户   1 超级用户\n\n");
+	printf("请选择：");
+	scanf("%d",&user[ui].attr);
+	getchar();
+	if(user[ui].attr == 1)
+	{
+		printf("请输入超级用户验证码:");
+		scanf("%s",super2);
+		getchar();
+		if(strncmp(super2, super, 20) == 0)
+		{
+			printf("\n恭喜您成为wechat超级用户\n\n\n");
+			user[ui].exist = 0;//用户变为存在
+		}
+	}
+	else
+	{
+		printf("\n恭喜您成为wechat用户！\n\n\n");
+		user[ui].exist = 0;//用户变为存在
+	}
+	user[ui].flag = -1; //初始都是不在线
+	user[ui].con_fd= -1; //初始化通信套接字都是-1
+	//fseek(fp, 0, SEEK_END);
+	//写入文件
+	//依次为账号， 密码， 姓名， 年龄， 性别， 个性签名， 属性， 是否存在， 通信连接套接字， 是否在线。
+	fprintf(fp,"%s %s %s %d %s %s %d %d %d %d\n",user[ui].number, user[ui].secret,user[ui].name, user[ui].age, user[ui].sex, user[ui].aut, user[ui].attr, user[ui].exist, user[ui].con_fd, user[ui].flag);
+	printf("正在返回主界面，请稍后...\n");
+	sleep(2);
+
+	face();
+
+	fclose(fp);
+}
+
+//选项
+void select_content(void)
+{
+	int s;
+
+	//initscr();
+	//box(stdscr,ACS_VLINE, ACS_HLINE);
+	system("clear");
+	printf("\n\n");
+	printf("\t\t\t     ---------- ---- -----\n");
+	printf("\t\t\t     |•ิ.•ัMr wang         |\n");
+	printf("\t\t\t     | ◙ ◈ ♫ ♬ ♪ ♩ ♭ ♪   |\n");
+	printf("\t\t\t     ---- ------ ---------\n\n");
+	printf("\t\t\t\t1 ~我 的 好 友~\n\n\n");
+	printf("\t\t\t\t2 ~在 线 好 友~\n\n\n");
+	printf("\t\t\t\t3 ~我  的  群 ~\n\n\n");
+	printf("\t\t\t\t4 ~聊  天  群 ~\n\n\n");
+	printf("\t\t\t\t5 ~  私   聊 ~\n\n\n");
+	printf("\t\t\t\t6 ~  群   聊 ~\n\n\n");
+	printf("\t\t\t\t7 ~修 改 资 料~\n\n\n");
+	printf("\t\t\t\t0 ~  退   出 ~\n\n\n\n");
+	printf("\t\t请选择：");
+	scanf("%d",&s);
+	getchar();
+	switch(s)
+	{
+		case 1:
+			see_friend();
+			break;
+		case 2:
+			see_live_friend();
+			break;
+		case 3:
+			see_group();
+			break;
+		case 4:
+			see_live_group();
+			break;
+		case 5:
+			create_client();
+			break;
+		case 6:
+			enter_group();
+			break;
+		case 7:
+			mod_identify();
+			break;
+		case 0: 
+			system("clear");
+			printf("\n\n\n\n\n\n\n\n\n\t\t\t\t谢谢使用，再见！！...\n\n\n\n");
+			break;
+		default:
+			printf("\n\n\t\t\t\t输入有误,请重新出入..\n");
+			sleep(1);
+			system("clear");
+			select_content();
+			break;
+	}
+	return;
+}
+
+void see_friend()
+{
+	int s,i;
+	char my_friend[10][20];
+	FILE *fp;
+	char u_number1[20];
+	char u_number2[20];
+
+	fp = fopen("friend.txt","r");
+	for(i = 0; i < 10; i++)
+	{
+		fscanf(fp, "%s\n", my_friend[i]);
+	}
+
+	system("clear");
+	printf("\n\n\n");
+	fflush(stdout);
+	printf("\n\t\t\t\t   我的好友\n\n\n");
+	for(i = 0; i < 10; i++)
+	{
+		printf("\t\t\t\t%d ☆ ★ %s\n\n",i+1, my_friend[i]);
+	}
+	printf("  1 添加好友\n");
+	printf("  2 删除好友\n\n");
+	printf("请选择：");
+	scanf("%d",&s);
+	switch(s)
+	{
+		case 1:
+			printf("请输入添加号码:");
+			scanf("%s",u_number1);
+			printf("添加成功!\n");
+			break;
+		case 2:
+			printf("请输入删除号码:");
+			scanf("%s",u_number2);
+			printf("删除成功!\n");
+			break;
+		default:
+			printf("输入错误，返回上一项...\n");
+	}
+
+	fclose(fp);
+}
+
+void see_live_friend()
+{
+
+}
+
+void see_group()
+{
+	int s;
+	char g_number1[20];
+	char g_number2[20];
+
+	printf("  1 添加群\n\n");
+	printf("  2 删除群\n");
+	switch(s)
+	{
+		case 1:
+			printf("添加群号:\n");
+			scanf("%s",g_number1);
+			break;
+		case 2:
+			printf("删除群号:\n");
+			scanf("%s",g_number2);
+			break;
+	}
+}
+
+void see_live_group()
+{
+}
+
+void create_client()
+{
+	private_chat(0);
+}
+
+void enter_group()
+{
+	private_chat(1);
+}
+
+void mod_identify()
+{
+
+}
